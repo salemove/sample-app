6d971e96f85e58c8a295a3604f6ef12137442e18
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-11 20:17:01 +0300

    Add feedback to "Talk to a human!" button
    Clicking it now starts a loading animation and the button is hidden once the
    Engagement starts. Also, the video won't fill an empty spot, rather it replaces
    a static Operator image that's added here.

--- sample.js
+++ sample.js
@@ -2,6 +2,11 @@
 const button = document.querySelector('.talk-to-human');
 button.addEventListener('click', () => {
   sm.getApi({version: 'v1'}).then((salemove) => {
-    salemove.requestEngagement('video');
+    const request = salemove.requestEngagement('video');
+    button.classList.add('loading');
+    request.engagementPromise.then((engagement) => {
+      button.classList.remove('loading');
+      button.classList.add('hidden');
+    });
   });
 });
